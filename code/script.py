b6f844e5a9d2495f5d8d31e25aa7292074a8b5cc
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-09 08:21:40 -0600

    Insert palette

--- code/script.py
+++ code/script.py
@@ -9,3 +9,17 @@ import urwid as uw
 def exit_on_q(key):
     if key in ('q', 'Q'):
         raise uw.ExitMainLoop()
+
+palette = [
+    ('banner', 'black', 'light gray'),
+    ('streak', 'black', 'dark red'),
+    ('bg', 'black', 'dark blue'),
+]
+
+
+text = uw.Text(('banner', u'Hello World'), align='center')
+map1 = uw.AttrMap(text, 'streak')
+fill = uw.Filler(map1)
+map2 = uw.AttrMap(fill, 'bg')
+loop = uw.MainLoop(map2, palette, unhandled_input=exit_on_q)
+loop.run()

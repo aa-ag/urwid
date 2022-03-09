0f903c653ae70cb4f43c37bf8a20debfc9ea68c0
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-09 08:16:35 -0600

    Add show_or_exit function from tutorial passed to MainLoop as an unhandled_input parameter

--- code/script.py
+++ code/script.py
@@ -6,12 +6,13 @@ import urwid as uw
 
 
 ############------------ FUNCTION(S) ------------############
-def testing_stuff():
-    text = uw.Text(u'Hello World')
-    fill = uw.Filler(text, 'top')
-    loop = uw.MainLoop(fill)
-    loop.run()
+def show_or_exit(key):
+    if key in ('q', 'Q'):
+        raise uw.ExitMainLoop()
+    text.set_text(repr(key))
 
-############------------ DRIVER CODE ------------############
-if __name__ == "__main__":
-    testing_stuff()
+
+text = uw.Text(u'Hello World')
+fill = uw.Filler(text, 'top')
+loop = uw.MainLoop(fill, unhandled_input=show_or_exit)
+loop.run()

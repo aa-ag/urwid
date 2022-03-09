ac4c3956ae945bd5286a26d057a75d1e93698eca
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-09 08:09:59 -0600

    Refactor structure for readability

--- code/script.py
+++ code/script.py
@@ -1,6 +1,17 @@
+############------------ IMPORTS ------------############
 import urwid as uw
 
-text = uw.Text(u'Hello World')
-fill = uw.Filler(text, 'top')
-loop = uw.MainLoop(fill)
-loop.run()
+
+############------------ GLOBAL VARIABLE(S) ------------############
+
+
+############------------ FUNCTION(S) ------------############
+def testing_stuff():
+    text = uw.Text(u'Hello World')
+    fill = uw.Filler(text, 'top')
+    loop = uw.MainLoop(fill)
+    loop.run()
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    testing_stuff()

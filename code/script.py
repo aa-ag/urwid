f11e4a9443ed2aafdf87b6bc9bc49613be6f6c6f
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-10 08:18:26 -0600

    Add keypress method logic

--- code/script.py
+++ code/script.py
@@ -2,13 +2,19 @@
 import urwid as uw
 
 
-############------------ GLOBAL VARIABLE(S) ------------############
-
-
 ############------------ FUNCTION(S) ------------############
 def exit_on_q(key):
     if key in ('q', 'Q'):
         raise uw.ExitMainLoop()
 
 class QuestionBox(uw.Filler):
-    pass
+    def keypress(self, size, key):
+        if key != 'enter':
+            return super(
+                QuestionBox, self
+            ).keypress(
+                size, key
+            )
+        self.original_widget = uw.Text(
+            u"Nice to meet you,\n%s.\n\nPress Q/q to exit." % edit.edit_text
+        )

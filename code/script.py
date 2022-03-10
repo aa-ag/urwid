4c1568d09dbae384f8052b06e064c2bce4b1b2de
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-10 08:18:45 -0600

    Insert driver logic

--- code/script.py
+++ code/script.py
@@ -18,3 +18,8 @@ class QuestionBox(uw.Filler):
         self.original_widget = uw.Text(
             u"Nice to meet you,\n%s.\n\nPress Q/q to exit." % edit.edit_text
         )
+
+edit = uw.Edit(u"What is your name?\n")
+fill = QuestionBox(edit)
+loop = uw.MainLoop(fill, unhandled_input=exit_on_q)
+loop.run()

65d2758d9e1b36df7863deac516f8e40902355e7
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-10 08:45:43 -0600

    Refactore script's structure to have global variables section again with set up for new logic

--- code/script.py
+++ code/script.py
@@ -2,24 +2,17 @@
 import urwid as uw
 
 
+############------------ GLOBAL VARIABLE(S) ------------############
+palette = [('I say', 'default,bold', 'default', 'bold'),]
+ask = uw.Edit(('I say', u"What is your name?\n"))
+reply = uw.Text(u"")
+button = uw.Button(u'Exit')
+div = uw.Divider()
+pile = uw.Pile([ask, div, reply, div, button])
+top = uw.Filler(pile, valign='top')
+
+
 ############------------ FUNCTION(S) ------------############
-def exit_on_q(key):
-    if key in ('q', 'Q'):
-        raise uw.ExitMainLoop()
 
-class QuestionBox(uw.Filler):
-    def keypress(self, size, key):
-        if key != 'enter':
-            return super(
-                QuestionBox, self
-            ).keypress(
-                size, key
-            )
-        self.original_widget = uw.Text(
-            u"Nice to meet you,\n%s.\n\nPress Q/q to exit." % edit.edit_text
-        )
 
-edit = uw.Edit(u"What is your name?\n")
-fill = QuestionBox(edit)
-loop = uw.MainLoop(fill, unhandled_input=exit_on_q)
-loop.run()
+############------------ DRIVER CODE ------------############

898fec307e453aea2fd40a47c30e7ecb60a6be62
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-12 19:12:45 -0600

    Add menu logic from tutorial

--- code/menu.py
+++ code/menu.py
@@ -9,16 +9,30 @@ options = u'A B C D'.split()
 
 
 ############------------ FUNCTION(S) ------------############
-def menu():
+def menu(title, options):
+    body = [uw.Text(title), uw.Divider()]
+    for option in options:
+        button = uw.Button(option)
+        uw.connect_signal(
+            button, 
+            'click', 
+            item_chosen, 
+            option
+        )
+        body.append(uw.AttrMap(
+            button,
+            None,
+            focus_map='reversed'
+        ))
+    return uw.ListBox(uw.SimpleFocusListWalker(body))
+
+
+def item_chosen():
     pass
 
 
-def item_chose():
-    pass
-
-
-def exit_program():
-    pass
+def exit_program(button):
+    return uw.ExitMainLoop()
 
 
 ############------------ DRIVER CODE ------------############

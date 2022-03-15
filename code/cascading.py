6c10551913543bba38fc21b820e42a58f44d1ef6
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-15 18:18:50 -0500

    Add open_box() metho in CascadingBoxes

--- code/cascading.py
+++ code/cascading.py
@@ -1,4 +1,5 @@
 ############------------ IMPORTS ------------############
+from ensurepip import bootstrap
 import urwid as uw
 
 
@@ -59,3 +60,28 @@ menu_top = menu(u'Main Menu', [
 ])
 
 ############------------ DRIVER CODE ------------############
+class CascadingBoxes(uw.WidgetPlaceholder):
+    max_box_levels = 4
+
+    def __init__(self, box):
+        super(CascadingBoxes, self).__init__(uw.SolidFill(u'/'))
+        self.box_level = 0
+        self.open_box(box)
+
+    def open_box(self, box):
+        self.original_widget = uw.Overlay(
+            uw.LineBox(box),
+            self.original_widget,
+            align='center', 
+            width=('relative', 80),
+            valign='middle', 
+            height=('relative', 80),
+            min_width=24, 
+            min_height=8,
+            left=self.box_level * 3,
+            right = (self.max_box_levels - self.box_level - 1) * 3,
+            top=self.box_level * 2,
+            bottom = (self.max_box_levels - self.box_level - 1) * 2
+        )
+        self.box_level += 1
+        

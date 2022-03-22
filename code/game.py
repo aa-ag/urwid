2e4c93273da80d8a8d62199228207b2368a7007b
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-21 21:22:13 -0500

    Add class ActionButton

--- code/game.py
+++ code/game.py
@@ -6,6 +6,11 @@ import urwid as uw
 
 
 ############------------ FUNCTION(S) ------------############
-
+class ActionButton(urwid.Button):
+    def __init__(self, caption, callback):
+        super(ActionButton, self).__init__("")
+        uw.connect_signal(self, 'click', callback)
+        self._w = uw.AttrMap(uw.SelectableIcon(caption, 1),
+            None, focus_map='reversed')
 
 ############------------ DRIVER CODE ------------############
